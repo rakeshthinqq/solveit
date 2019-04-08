7e95de080e18893e08c8c2dfaca5d745cb1f4376
rakeshthinqq <rakesh@thinqq.com>
2019-04-08 11:49:21 -0700

    shuffle cards problem to check if its single riffle

---
+++ src/main/java/com/solveit/problems/arrays/ShuffleCards.java
@@ -0,0 +1,121 @@
+package com.solveit.problems.arrays;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class ShuffleCards {
+
+    public static boolean isSingleRiffle(int[] half1, int[] half2, int[] shuffledDeck) {
+
+        /**
+         *
+         * 1    Iterated Shuffled set
+         * 2        take 1 and see if that is part of half1
+         *          then
+         *              incremtent left pointer
+         *           else check if its part of Right
+         *           then
+         *              increment right pointer
+         *           else
+         *              its not singele rffle
+         *
+         *
+         */
+
+
+        int leftPointer = 0;
+        int rightPointer = 0;
+
+        for(int deckCard : shuffledDeck) {
+
+            if(leftPointer < half1.length && deckCard == half1[leftPointer]) {
+                ++leftPointer;
+            } else if(rightPointer < half2.length && deckCard == half2[rightPointer] ) {
+                ++rightPointer;
+            } else {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void bothHalvesAreTheSameLengthTest() {
+        final int[] half1 = {1, 4, 5};
+        final int[] half2 = {2, 3, 6};
+        final int[] shuffledDeck = {1, 2, 3, 4, 5, 6};
+        final boolean result = isSingleRiffle(half1, half2, shuffledDeck);
+        assertTrue(result);
+    }
+
+    @Test
+    public void halvesAreDifferentLengthsTest() {
+        final int[] half1 = {1, 5};
+        final int[] half2 = {2, 3, 6};
+        final int[] shuffledDeck = {1, 2, 6, 3, 5};
+        final boolean result = isSingleRiffle(half1, half2, shuffledDeck);
+        assertFalse(result);
+    }
+
+    @Test
+    public void oneHalfIsEmptyTest() {
+        final int[] half1 = {};
+        final int[] half2 = {2, 3, 6};
+        final int[] shuffledDeck = {2, 3, 6};
+        final boolean result = isSingleRiffle(half1, half2, shuffledDeck);
+        assertTrue(result);
+    }
+
+    @Test
+    public void shuffledDeckIsMissingCardsTest() {
+        final int[] half1 = {1, 5};
+        final int[] half2 = {2, 3, 6};
+        final int[] shuffledDeck = {1, 6, 3, 5};
+        final boolean result = isSingleRiffle(half1, half2, shuffledDeck);
+        assertFalse(result);
+    }
+
+    @Test
+    public void shuffledDeckHasExtraCards() {
+        final int[] half1 = {1, 5};
+        final int[] half2 = {2, 3, 6};
+        final int[] shuffledDeck = {1, 2, 3, 5, 6, 8};
+        final boolean result = isSingleRiffle(half1, half2, shuffledDeck);
+        assertFalse(result);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(ShuffleCards.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
